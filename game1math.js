4f1b7b217159ab66e5157dfaead42e63de9145ec
ivsong0m3s <143664644+ivsong0m3s@users.noreply.github.com>
2024-05-22 09:51:23 -0300

    newmath

--- game1math.js
+++ game1math.js
@@ -1,5 +1,34 @@
-var entrada = require ('readline-sync')
+var entrada = require ("readline-sync");
 
 var numb = Math.floor(Math.random() *100);
 
+var tentativa = 0;
+console.log(numb);
+var tentativa = entrada.question(`Advinhe qual e o numero? `);
 
+while(tentativa<=100){
+    if (tentativa<numb){
+        console.log(`Você errou! Seu numero é menor`);
+        break
+    }
+
+    if(tentativa>numb){
+        console.log(`Voce errou! Seu numero é maior`);
+        break
+    }
+
+var resposta = entrada.question(`Voce quer jogar novamente? Digite S ou N: `)
+    if (resposta.toUpperCase() === 'N') {
+        console.log("Você saiu do jogo.");
+
+    } else if (resposta.toUpperCase() !== 'S') {
+        console.log("Palavra inválida. Digite S ou N.");
+
+
+
+   }
+
+}
+
+    
+    
