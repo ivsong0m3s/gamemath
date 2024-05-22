b5aa9bea4a81f72197567700b47656eae215bbd7
ivsong0m3s <143664644+ivsong0m3s@users.noreply.github.com>
2024-05-22 10:51:38 -0300

    concluido

--- game1math.js
+++ game1math.js
@@ -1,12 +1,17 @@
 var entrada = require ("readline-sync");
 
 var numb = Math.floor(Math.random() *100);
+var tentativa=0;
+var contador=0;
 
-var tentativa = 0;
+var novamente = true;
+while (novamente) {
+  
 console.log(numb);
 var tentativa = entrada.question(`Advinhe qual e o numero? `);
 
 while(tentativa<=100){
+   
     if (tentativa<numb){
         console.log(`Você errou! Seu numero é menor`);
         break
@@ -16,19 +21,18 @@ while(tentativa<=100){
         console.log(`Voce errou! Seu numero é maior`);
         break
     }
-
-var resposta = entrada.question(`Voce quer jogar novamente? Digite S ou N: `)
-    if (resposta.toUpperCase() === 'N') {
-        console.log("Você saiu do jogo.");
-
-    } else if (resposta.toUpperCase() !== 'S') {
-        console.log("Palavra inválida. Digite S ou N.");
-
-
-
-   }
-
+    if(tentativa = numb){
+        console.log(`Voce Acertou!!!, Voce tentou, ${contador} vezes`);
+        break
+    }
+    contador++;
 }
-
-    
-    
+var resposta = entrada.question(`Você quer jogar novamente? Digite S ou N: `);
+if (resposta.toUpperCase() === 'N') {
+    console.log("Você saiu do jogo.");
+    novamente = false;
+} else if (resposta.toUpperCase() !== 'S') {
+    console.log("Palavra inválida. Digite S ou N.");
+    break
+}
+}
